3c513b3be5876324378567c934bf8a7b21017e89
Taylor Caldwell <tcaldwell@twitter.com>
2025-03-21 11:06:35 -0700

    dasa

--- app.py
+++ app.py
@@ -396,7 +396,7 @@ def refresh_oauth_token(token):
     if 'refresh_token' not in token:
         return {'error': 'No refresh token available'}
     
-    refresh_token = token['refresh_token'] + '1234567890'
+    refresh_token = token['refresh_token']
     
     # Create a new OAuth session
     x_session = OAuth2Session(X_CLIENT_ID)
