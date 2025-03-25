c29c11218fe82d12d5a8caa1eff901c22be970af
Taylor Caldwell <tcaldwell@twitter.com>
2025-03-25 11:47:34 -0700

    ....

--- app.py
+++ app.py
@@ -405,8 +405,8 @@ def refresh_oauth_token(token):
     token_data = {
         'grant_type': 'refresh_token',
         'refresh_token': refresh_token,
-        'client_id': X_CLIENT_ID,
-        'client_secret': X_CLIENT_SECRET
+        'client_id': X_CLIENT_ID
+        # 'client_secret': X_CLIENT_SECRET
     }
     
     try:
